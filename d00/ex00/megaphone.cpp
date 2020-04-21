e2bfb830712c903bdcb727764677ac4ae89d9292
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 21:35:55 +0200

    Not sure why the input in the func param is &c in the declaration

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <algorithm>
+
+void test_func(char& c) {
+	c = 'x';
+}
 
 int main(int ac, char* av[]) {
 	if (ac == 1) {
 		return (0);
 	}
 
-	int i = 1;
-	int j = 0;
+	std::string test = av[1];
+	// test += av[2];
 
-	while (i < ac) {
-		//code
-		i++;
-	}
+	std::for_each(test.begin(), test.end(), test_func);
+	std::cout << test;
 
 	return (0);
 }
