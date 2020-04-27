f395a022ab1ce570232b823c8218ddf6dae6cf08
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-27 10:53:32 +0200

    notes

--- d00/vids/class_instance.cpp
+++ d00/vids/class_instance.cpp
@@ -1,1 +1,6 @@
 // see Sample.class.cpp in test dir
+// see end of this vid for a small ref to dynamic mem allocation
+// for classes.
+// I think this is stack mem allocation but this seems weird, I could
+// be missing some basic concept here, will see how to manage mem 
+// with classes with in the scope of the program
