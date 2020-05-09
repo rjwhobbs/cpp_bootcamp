fa14f2a2eb863149d5fbf26665b9705c25e715c1
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-05-09 22:25:25 +0200

    code

--- d00/test_ex02/vector_test.cpp
+++ d00/test_ex02/vector_test.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main(void) {
-	vector<int> int_v(5, 1);
+	int const 		dep[]	= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	size_t const 	dep_size( sizeof(dep) / sizeof(int) );
+	vector<int> 	int_v(dep, dep + dep_size); // with this constructor it takes pointers to the begining and end of the array.
 	vector<int>::iterator b = int_v.begin();
 	vector<int>::iterator e = int_v.end();
 
-	while (b != e) {
-		cout << *b << endl;
-		b++;
-	}
+	cout << (dep_size) << endl;
+	cout << (dep) << endl;
+	// while (b != e) {
+	// 	cout << *b << endl;
+	// 	b++;
+	// }
 	return 0;
 }
