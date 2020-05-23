0104d80253782738837e4b156736071ab8a67cf8
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-05-23 16:14:52 +0200

    So the tm struct only holds the amount of years months etc since the epoch

--- d00/test_ex02/Acc.class.cpp
+++ d00/test_ex02/Acc.class.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Acc.class.hpp"
 
-Acc::Acc(int initial_deposit) {
+Acc::Acc(int initial_deposit) 
+{
 	this->_amount = initial_deposit;
 	this->_accountIndex = Acc::_nbAccounts;
 	this->_nbDeposits = 0;
@@ -15,7 +16,8 @@ Acc::Acc(int initial_deposit) {
 	return ;
 }
 
-Acc::Acc(void) {
+Acc::Acc(void) 
+{
 	this->_amount = 0;
 	this->_accountIndex = Acc::_nbAccounts;
 	this->_nbDeposits = 0;
@@ -29,7 +31,8 @@ Acc::Acc(void) {
 	return ;
 }
 
-void Acc::makeDeposit(int deposit) {
+void Acc::makeDeposit(int deposit) 
+{
 	int p_amount = this->_amount;
 	this->_amount += deposit;
 	this->_nbDeposits += 1;
@@ -42,17 +45,20 @@ void Acc::makeDeposit(int deposit) {
 	std::cout << "nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
-bool Acc::makeWithdrawal(int withdrawal) {
+bool Acc::makeWithdrawal(int withdrawal) 
+{
 	int p_amount = this->_amount;
 
-	if (withdrawal > this->checkAmount()) {
+	if (withdrawal > this->checkAmount()) 
+	{
 		std::cout << "index:" << this->_accountIndex << ';';
 		std::cout << "p_amount:" << p_amount << ';';
 		std::cout << "withdrawal:refused" << std::endl;
 
 		return false;
-
-	} else if (withdrawal > 0) {
+	} 
+	else if (withdrawal > 0) 
+	{
 		this->_amount -= withdrawal;
 		this->_nbWithdrawals += 1;
 		Acc::_totalNbWithdrawals += 1;
@@ -73,18 +79,21 @@ bool Acc::makeWithdrawal(int withdrawal) {
 	return false;
 }
 
-int Acc::checkAmount(void) const {
+int Acc::checkAmount(void) const 
+{
 	return this->_amount;
 }
 
-void Acc::displayStatus(void) const {
+void Acc::displayStatus(void) const 
+{
 	std::cout << "index:" << this->_accountIndex << ';';
 	std::cout << "amount:" << this->_amount << ';';
 	std::cout << "nb_deposits:" << this->_nbDeposits << ';';
 	std::cout << "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
-Acc::~Acc(void) {
+Acc::~Acc(void) 
+{
 	std::cout << "index:" << this->_accountIndex << ';';
 	std::cout << "amount:" << this->_amount << ';';
 	std::cout << "closed" << std::endl;
