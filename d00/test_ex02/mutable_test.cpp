1285494958b84347a8831f40aaed04d5dfa6006a
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-05-26 19:17:30 +0200

    So static and mutable vars can be changed by a const member function

--- d00/test_ex02/mutable_test.cpp
+++ d00/test_ex02/mutable_test.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 class Test {
 	public: 
-	static int x;	
+	static int x;
+	mutable int y;	
 	void f(void) const;
 };
 
@@ -11,7 +12,9 @@ class Test {
 int Test::x = 42;
 void Test::f(void) const {
 	this->x += 1;
+	this->y = 9000;
 	cout << this->x << endl;
+	cout << this->y << endl;
 }
 
 int main(void) {
