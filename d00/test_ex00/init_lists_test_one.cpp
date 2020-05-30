2056fb95ddc27537daf350303757b019c2ec707d
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-05-30 19:47:57 +0200

    Some init list with inheritance

--- d00/test_ex00/init_lists_test_one.cpp
+++ d00/test_ex00/init_lists_test_one.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 using namespace std;
 
-class Test 
+class TestOne 
 {
 public:
-	Test(int x, int y);
-	~Test();
+	TestOne(int x, int y);
+	~TestOne();
 
 	void putXY(void) const;
 	
@@ -15,22 +15,39 @@ private:
 	int _y;
 };
 
-Test::Test(int x, int y) : _x(x), _y(y)
+TestOne::TestOne(int x, int y) : _x(x), _y(y)
 {
 	cout << this->_x << ' ' << this->_y << endl;
+	cout << "From TestOne constructor" << endl;
 }
 
-Test::~Test(){}
+TestOne::~TestOne(){}
 
-void Test::putXY(void) const
+void TestOne::putXY(void) const
 {
 		cout << this->_x << ' ' << this->_y << endl;
 }
 
+/* ------------------------------------------------ */
+
+class TestTwo: public TestOne
+{
+public:
+	TestTwo();
+	
+};
+
+TestTwo::TestTwo(void) : TestOne(65, 54)
+{
+		cout << "From TestTwo constructor" << endl;
+}
+
 int main(void)
 {
-	vector<Test> test_v(5, Test(21, 43));
-	test_v[0].putXY();
-	test_v[4].putXY();
+	// vector<TestOne> test_v(5, TestOne(21, 43));
+	// test_v[0].putXY();
+	// test_v[4].putXY();
+	TestTwo t;
+	t.putXY();
 	return 0;
 }
