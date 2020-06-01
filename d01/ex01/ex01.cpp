7f15f5cfe787ad7ed2cafb7761e0680d04f3112f
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-06-01 19:18:21 +0200

    ex01

---
+++ d01/ex01/ex01.cpp
@@ -0,0 +1,10 @@
+// Fix mem leak 
+#include <string>
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << panthere << std::endl;
+	delete panthere;
+}
