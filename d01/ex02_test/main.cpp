2d4654a2459f49ad2e2557e27fcac1c71b446ed8
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-06-03 20:10:29 +0200

    basic random Zombie name function

--- d01/ex02_test/main.cpp
+++ d01/ex02_test/main.cpp
@@ -1,12 +1,38 @@
+#include <cstdlib>
 #include <iostream>
+#include <stdlib.h>
 #include "Zombie.hpp"
 #include "ZombieEvent.hpp"
+
+int random_index(int max) {
+	sranddev();
+	int ranNum = rand() % max;
+
+	return ranNum;
+}
+
+std::string zombieNameGen(int index) {
+	std::string names[10] = {
+		"Steve", "Donald Trump", "Micheal J. Fox", "Lillian", "X",
+		"Barbara", "Zoe", "The Dude", "Default Name", "The President of Cheese"};
+
+	return names[index];
+}
+
+void randomChump(void) {
+	Zombie randomZombie("stack Zombie", zombieNameGen(random_index(10)));
+	randomZombie.announce();
+}
 
 int main(void) {
 	ZombieEvent betterThanAverageZombieEvent;
 	betterThanAverageZombieEvent.setZombieType("better than average");
 	Zombie* joe = betterThanAverageZombieEvent.newZombie("Joe");
+	Zombie* steve = betterThanAverageZombieEvent.newZombie("Steve");
 	joe->announce();
+	steve->announce();
+	randomChump();
 	delete joe;
+	delete steve;
 	return 0;
 }
