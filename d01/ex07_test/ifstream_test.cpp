bc1fb6ba869cc78b30f2c8e40f1acb6da4903215
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-06-16 15:23:55 +0200

    Basic error handling but also need to check file perms

--- d01/ex07_test/ifstream_test.cpp
+++ d01/ex07_test/ifstream_test.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <sstream>
 #include <iostream>
 #include <fstream>
@@ -17,21 +18,39 @@ int main(void) {
 	std::string tempStr;
 	std::ifstream ifs("test.txt");
 
+	if (ifs.fail()) {
+		std::cerr << "Error reading file.\nEnsure that the path is correct and that you have permission to read and write to it." << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	streamBuf << ifs.rdbuf();
 	tempStr = streamBuf.str();
 
 	size_t pos = tempStr.find(find, 0);
 	while (pos != std::string::npos) {
-		tempStr.replace(pos, find.length(), replace);
-		pos = tempStr.find(find, pos + replace.length());
+		if (pos > 0) {
+			if (isNonWord(tempStr[pos - 1]) && 
+					isNonWord(tempStr[find.length() + pos])) 
+			{
+				tempStr.replace(pos, find.length(), replace);
+				pos = tempStr.find(find, pos + replace.length());
+			} else {
+				pos = tempStr.find(find, pos + find.length());
+			}
+		} else if (pos == 0) {
+			if (isNonWord(tempStr[find.length()])) {
+				tempStr.replace(pos, find.length(), replace);
+				pos = tempStr.find(find, pos + replace.length());
+			} else {
+				pos = tempStr.find(find, pos + find.length());
+			}
+		}
 	}
 
-	std::cout << tempStr << std::endl;
-
-
-	// std::ofstream ofs("");
+	std::ofstream ofs("test.txt", std::ofstream::trunc);
+	ofs << tempStr;
 
 	ifs.close();
-	// ofs.close();
+	ofs.close();
 	return 0;
 }
