780f42c0eddf47e1f7ed931a6088dd3a17bb68b9
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-06-16 08:56:34 +0200

    Playing around with refs and pointers

--- d01/ex06_test/ptr_ref_test.cpp
+++ d01/ex06_test/ptr_ref_test.cpp
@@ -16,8 +16,40 @@ class Student {
 		string* _login;
 };
 
+class StudentTwo {
+	private:
+		string _login;
+
+	public:
+		StudentTwo(string const& login) : _login(login) {}
+		~StudentTwo(void) {}
+
+		string& getLoginRef(void) {
+			return this->_login;
+		}
+
+		string const& getLoginRefConst(void) const {
+			return this->_login;
+		}
+
+		string* getLoginPtr(void) {
+			return &(this->_login);
+		}
+
+		string const* getLoginPtrConst(void) const {
+			return &(this->_login);
+		}
+};
+
 int main(void) {
 	string name = "Potato";
+	StudentTwo stu = StudentTwo("Jack");
+
+	cout << *(stu.getLoginPtrConst()) << endl; 
+	*(stu.getLoginPtr()) = "The dude"; 
+	cout << *(stu.getLoginPtrConst()) << endl; 
+	stu.getLoginRef() = "The Chana"; 
+	cout << *(stu.getLoginPtrConst()) << endl; 
 
 	Student student(name);
 	cout << student.getLogin() << endl;
